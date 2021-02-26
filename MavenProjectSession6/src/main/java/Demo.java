b5a030cbd3377d6eff43a62729e0f712e2edad6c
Parth Choudhary <78471699+Parth-C8@users.noreply.github.com>
2021-02-26 10:58:41 +0530

    Add files via upload

---
+++ MavenProjectSession6/src/main/java/Demo.java
@@ -0,0 +1,12 @@
+
+import org.springframework.*;
+
+/*
+    This class imports spring framework successfully without any error !!
+ */
+public class Demo {
+    public static void main(String[] args) {
+
+        System.out.println("\nWelcome");
+    }
+}
